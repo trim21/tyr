78d8f1ff62778a2994489cfa0a76120d9a5513e6
Trim21 <trim21.me@gmail.com>
2024-06-26 20:49:49 +0800

    fix

--- internal/web/web.go
+++ internal/web/web.go
@@ -54,7 +54,7 @@ func New(c *core.Client, token string, enableDebug bool) http.Handler {
 	r := chi.NewMux()
 	r.Use(middleware.Recoverer)
 
-	r.Get("/ping", func(w http.ResponseWriter, r *http.Request) {
+	r.Get("/healthz", func(w http.ResponseWriter, r *http.Request) {
 		res.Text(w, http.StatusOK, ".")
 	})
 
